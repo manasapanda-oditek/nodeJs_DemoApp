8c9bb1800769b8a6d4490fd7dfde5470016cd0ec
manasapanda-oditek <manasapanda2020@gmail.com>
2024-05-20 17:48:30 +0530

    adding some design

--- app.js
+++ app.js
@@ -6,19 +6,30 @@ app.use(express.json());
 
 require('dotenv').config();
 
-app.get("/",(req,res)=>{
-    res.status(200).send("Welcome to node js")
-})
+app.set("view engine","ejs");
 
-app.get("/details",(req,res)=>{
-    res.status(200).send("This is details page");
-})
+app.use(express.static('./public'));
 
-app.get("/about",(req,res)=>{
-    res.status(200).send("About page")
-})
-
-
+// app.get('/',(req, res)=>{
+//   res.send('welcome to home');
+// });
+app.get('/profile',(req, res)=>{
+  res.send('welcome to profile');
+});
+app.get('/profile/:name',(req, res)=>{
+  const uname=req.params.name;
+  res.send(`welcome to profile ${uname}`);
+});
+app.get('/',(req,res)=>{
+  res.render("index");
+});
+app.get('/contact',(req,res)=>{
+  // res.send();
+  res.render("contact",{Contact: "YOO THE BOY ANGULAR SOMU"});
+});
+app.get('/details',(req,res)=>{
+  res.render("details",{Hover :"manasa panda"});
+});
 
 
 const port= process.env.PORT || 5000
